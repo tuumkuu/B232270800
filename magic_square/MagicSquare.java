fbc69ce37de5684b858ece5873a352c36f4d2728
tuumkuu <74714757+tuumkuu@users.noreply.github.com>
2025-06-09 02:21:38 +0800

    Add files via upload

---
+++ magic_square/MagicSquare.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class MagicSquare {
+
+    private static final int[][][] MAGIC_SQUARES = {
+            { { 8, 1, 6 }, { 3, 5, 7 }, { 4, 9, 2 } },
+            { { 6, 1, 8 }, { 7, 5, 3 }, { 2, 9, 4 } },
+            { { 4, 9, 2 }, { 3, 5, 7 }, { 8, 1, 6 } },
+            { { 2, 9, 4 }, { 7, 5, 3 }, { 6, 1, 8 } },
+            { { 8, 3, 4 }, { 1, 5, 9 }, { 6, 7, 2 } },
+            { { 4, 3, 8 }, { 9, 5, 1 }, { 2, 7, 6 } },
+            { { 6, 7, 2 }, { 1, 5, 9 }, { 8, 3, 4 } },
+            { { 2, 7, 6 }, { 9, 5, 1 }, { 4, 3, 8 } }
+    };
+
+    public static int formingMagicSquare(int[][] s) {
+        int minCost = Integer.MAX_VALUE;
+
+        for (int[][] magic : MAGIC_SQUARES) {
+            int cost = 0;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+
+                    cost += Math.abs(s[i][j] - magic[i][j]);
+                }
+            }
+
+            minCost = Math.min(minCost, cost);
+        }
+
+        return minCost;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] s = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                s[i][j] = scanner.nextInt();
+            }
+        }
+
+        int result = formingMagicSquare(s);
+        System.out.println(result);
+    }
+}
